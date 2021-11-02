2f19b969ca1c321bf1f5d10993ca3895048cf80c
Cenk Berry <83925796+cenkberry@users.noreply.github.com>
2021-11-02 21:07:40 +0300

    Add files via upload

---
+++ js/change.js
@@ -0,0 +1,25 @@
+function changew() {
+
+    var mugBody = document.getElementById('mugBody');
+    var mugHandle = document.getElementById('mugHandle');
+
+    mugBody.style.backgroundColor = '#f8f8f8';
+    mugHandle.style.backgroundColor = '#f8f8f8';
+
+
+
+
+}
+
+function changeb() {
+
+    var mugBody = document.getElementById('mugBody');
+    var mugHandle = document.getElementById('mugHandle');
+
+    mugBody.style.backgroundColor = '#000000';
+    mugHandle.style.backgroundColor = '#000000';
+
+
+
+
+}
