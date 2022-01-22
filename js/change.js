0ce54e13e67a2a3c2e5e3d940eed2efe06728bd6
Cenk Beri <cenkberi@gmail.com>
2022-01-22 17:55:27 +0300

    clear code design

--- js/change.js
+++ js/change.js
@@ -1,25 +1,15 @@
 function changew() {
-
     var mugBody = document.getElementById('mugBody');
     var mugHandle = document.getElementById('mugHandle');
 
     mugBody.style.backgroundColor = '#f8f8f8';
     mugHandle.style.backgroundColor = '#f8f8f8';
-
-
-
-
 }
 
 function changeb() {
-
     var mugBody = document.getElementById('mugBody');
     var mugHandle = document.getElementById('mugHandle');
 
     mugBody.style.backgroundColor = '#000000';
     mugHandle.style.backgroundColor = '#000000';
-
-
-
-
 }
